726eba356931deb77156499433446a179d816385
lab04 <lab04@lab4-03>
2024-10-07 22:53:12 -0300

    Atualização adicionado abrir nova tela.

--- LojaProdutos/src/br/com/etec/model/Operacoes.java
+++ LojaProdutos/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,6 @@
 package br.com.etec.model;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,11 +8,16 @@ import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Operacoes {
 	@FXML
@@ -25,8 +31,9 @@ public class Operacoes {
 	@FXML
 	private Stage acpPalco;
 	
+	
 	@FXML
-	private void acessarConta(ActionEvent event) {
+	private void acessarConta(ActionEvent event) throws SQLException, IOException {
 		
 		String nomeUsuario;
 		nomeUsuario = txfUsuario.getText();
@@ -50,13 +57,15 @@ public class Operacoes {
 		} // if
 	
 		else {
-			if(nomeUsuario.equals("admin") && senhaUsuario.equals("123456")) {
+			if(verificarUsuarioSenha(nomeUsuario, senhaUsuario)) {
 				mostrarMensagem(Alert.AlertType.CONFIRMATION, 
 						"ACESSO PERMITIDO", "Logado no sistema.");
+				acessarTelaPrincipal(event);
 			}
 			else {
 				mostrarMensagem(Alert.AlertType.ERROR, 
 						"ERRO DE ACESSO", "Usuário ou senha errada.");
+						
 			}
 		}
 	} // acessarConta
@@ -79,36 +88,52 @@ public class Operacoes {
     //----------------------------------------------------------------	
 		
 	}
-	@FXML
-	private boolean verificarUsuarioSenha(String usuario, String senha) throws SQLException {
-        Connection conexao = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        boolean usuarioValido = false;
-
-        try {
-            conexao = Conexao.conectar();
-            String sql = "SELECT * FROM tabelalogin WHERE usuario = ? AND senha = ?";
-            stmt = conexao.prepareStatement(sql);
-            stmt.setString(1, usuario);
-            stmt.setString(2, senha);
-            rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                usuarioValido = true;
-            }
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            Conexao.fechar(conexao);
-        }
-
-        return usuarioValido;
-    }
 	
+
+	 private boolean verificarUsuarioSenha(String usuario, String senha) throws SQLException {
+	        Connection conexao = null;
+	        PreparedStatement stmt = null;
+	        ResultSet rs = null;
+	        boolean usuarioValido = false;
+
+	        try {
+	            conexao = Conexao.conectar();
+	            String sql = "SELECT * FROM login_tb WHERE usuario = ? AND senha = ?";
+	            stmt = conexao.prepareStatement(sql);
+	            stmt.setString(1, usuario);
+	            stmt.setString(2, senha);
+	            rs = stmt.executeQuery();
+
+	            if (rs.next()) {
+	                usuarioValido = true;
+	            }
+	        } finally {
+	            if (rs != null) {
+	                rs.close();
+	            }
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	            Conexao.fechar(conexao);
+	        }
+
+	        return usuarioValido;
+	    }
 	
+
+//-------------------------------------------------------------
+public void acessarTelaPrincipal(ActionEvent event) throws IOException{
+	AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/etec/view/telaPrincipal.fxml"));
+	
+	Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	
+	Scene scene = new Scene(root);
+	scene.getStylesheets().add(getClass().getResource("/br/com/etec/view/application.css").toExternalForm());
+	primaryStage.setScene(scene);
+	
+	primaryStage.initStyle(StageStyle.UNDECORATED);
+	
+	primaryStage.show();
+}
+
 }
