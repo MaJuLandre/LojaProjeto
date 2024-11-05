880591440e75b2d4b879e4ed74d557c96bd40393
lab04 <lab04@lab4-03>
2024-11-04 22:56:45 -0300

    Colocamos select no banco de dados

--- LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
+++ LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
@@ -1,12 +1,12 @@
 package br.com.etec.model;
 
+
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-
-
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -30,39 +30,35 @@ public class ClassePrincipal implements Initializable{
 	Button btnMarca;
 	@FXML
 	Button btnModelo;
-
-	
 	
 	public void initialize(URL url, ResourceBundle rb) {
-		ObservableList<String> lista =
-				FXCollections.observableArrayList("Chinelo", "Tênis");
-				cmbTipo.setItems(lista);
 		
-		ObservableList<String> lista2 =
-				FXCollections.observableArrayList
-				("Nike", "Adidas");
-				
-				cmbTipo.setItems(lista2);
-				
-				
-		ObservableList<String> lista3 =
-				FXCollections.observableArrayList
-				("TEN001", "CHN001", "TEA001","CHA001");
-						
-				cmbTipo.setItems(lista3);
+		/*
+		ObservableList<String> lista = FXCollections.observableArrayList("Chinelo", "Tênis");
+		cmbTipo.setItems(lista);
 		
+		ObservableList<String> lista2 = FXCollections.observableArrayList("Nike", "Adidas");
+		cmbTipo.setItems(lista2);
+		
+		ObservableList<String> lista3 = FXCollections.observableArrayList("TEN001", "CHN001", "TEA001", "CHA001");
+		cmbTipo.setItems(lista3);
+		*/
+		// Chamar o método com as informações
+		carregarComboBox(cmbTipo, "tipo");
+		carregarComboBox(cmbMarca, "marca");
+		carregarComboBox(cmbModelo, "modelo");
 	}
 	
-	
+	/*
 	// Método para inserir dados no banco de dados
     public void inserirDados(String valor) {
-        String sql = "INSERT INTO tabela (coluna1, coluna2, coluna3) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO produtos_tb (tipo, marca, modelo) VALUES (?, ?, ?)";
         
         Connection conn = null;
         PreparedStatement pstmt = null;
         
         try {
-            conn = Conexao.conectar();
+            conn = ClasseConexao.conectar();
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, tipoProduto);
             pstmt.setString(2, marcaProduto);
@@ -72,7 +68,7 @@ public class ClassePrincipal implements Initializable{
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
-        	Conexao.fechar(conn);
+        	ClasseConexao.fechar(conn);
             if (pstmt != null) {
                 try {
                     pstmt.close();
@@ -83,5 +79,26 @@ public class ClassePrincipal implements Initializable{
         }
     }
 	
+	*/
 	
+	private void carregarComboBox(ComboBox<String> comboBox, String coluna){
+		// Variável lista = collections (guardar várias informações)
+		ObservableList<String> lista = 
+				FXCollections.observableArrayList();
+		try (Connection conn = ClasseConexao.conectar()){
+			// Pra exutar o comando SQL e guardar na variável rs
+			String sql = "SELECT DISTINCT" + coluna + "FROM minhatabela";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				lista.add(rs.getString(coluna));
+			}
+			// Jpga os valores da lista na combobox
+			comboBox.setItems(lista);
+		} // fecha try
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	} // void
+
 }
